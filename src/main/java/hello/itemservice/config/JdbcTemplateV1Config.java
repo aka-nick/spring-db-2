68ec901565ce7ab1711fc6746d59d48dd1142867
aka-nick <bechalrang@gmail.com>
2023-03-11 16:22:24 +0900

    섹션2 - JdbcTemplate 이름지정 파라미터3 - 새 JdbcConfig 등록, unused imports 제거

--- src/main/java/hello/itemservice/config/JdbcTemplateV1Config.java
+++ src/main/java/hello/itemservice/config/JdbcTemplateV1Config.java
@@ -2,7 +2,6 @@ package hello.itemservice.config;
 
 import hello.itemservice.repository.ItemRepository;
 import hello.itemservice.repository.jdbctemplate.JdbcTemplateItemRepositoryV1;
-import hello.itemservice.repository.memory.MemoryItemRepository;
 import hello.itemservice.service.ItemService;
 import hello.itemservice.service.ItemServiceV1;
 import javax.sql.DataSource;
