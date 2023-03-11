c386dc381f45cb75668d696d759c3c1124e73d89
aka-nick <bechalrang@gmail.com>
2023-03-11 16:33:30 +0900

    섹션2 - JdbcTemplate - JdbcTemplateV3Config 적용

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -1,7 +1,6 @@
 package hello.itemservice;
 
-import hello.itemservice.config.JdbcTemplateV1Config;
-import hello.itemservice.config.JdbcTemplateV2Config;
+import hello.itemservice.config.JdbcTemplateV3Config;
 import hello.itemservice.repository.ItemRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,7 +9,8 @@ import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
 
-@Import(JdbcTemplateV2Config.class)
+@Import(JdbcTemplateV3Config.class)
+//@Import(JdbcTemplateV2Config.class)
 //@Import(JdbcTemplateV1Config.class)
 //@Import(MemoryConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
