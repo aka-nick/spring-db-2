779041fd9c59fa8780ff6341f3f2afc9a31be066
aka-nick <bechalrang@gmail.com>
2023-03-13 20:44:11 +0900

    섹션4 - 데이터 접근 기술 - SpringDataJPA - SpringDataJpa Repository interface 구현

---
+++ src/main/java/hello/itemservice/repository/jpa/SpringDataJpaItemRepository.java
@@ -0,0 +1,23 @@
+package hello.itemservice.repository.jpa;
+
+import hello.itemservice.domain.Item;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+
+public interface SpringDataJpaItemRepository extends JpaRepository<Long, Item> {
+
+    List<Item> findByItemNameLike(String itemName);
+
+    List<Item> findByPriceLessThanEqual(Integer price);
+
+    // 쿼리메서드
+//    List<Item> findByItemNameLikeAndPriceLessThanEqual(String itemName, Integer price);
+    // JPQL
+    @Query("select i from Item i where i.itemName like :itemName and i.price <= :price")
+    List<Item> findItems(@Param("itemName") String itemName, @Param("price") Integer price);
+
+
+}
