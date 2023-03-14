759404cb1e5ef64d2263b9cf2ba3d5ea1a9f5495
aka-nick <bechalrang@gmail.com>
2023-03-14 12:56:24 +0900

    섹션4 - 데이터 접근 기술 - QueryDSL - 트레이드오프 한 구조로 리팩토링(일반적인 query를 담당할 ItemRepositoryV2 생성)

--- src/main/java/hello/itemservice/repository/jpa/JpaItemRepositoryV3.java
+++ src/main/java/hello/itemservice/repository/jpa/JpaItemRepositoryV3.java
@@ -1,12 +1,11 @@
 package hello.itemservice.repository.jpa;
 
-import static hello.itemservice.domain.QItem.*;
+import static hello.itemservice.domain.QItem.item;
 
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import hello.itemservice.domain.Item;
-import hello.itemservice.domain.QItem;
 import hello.itemservice.repository.ItemRepository;
 import hello.itemservice.repository.ItemSearchCond;
 import hello.itemservice.repository.ItemUpdateDto;
--- src/main/java/hello/itemservice/repository/jpa/SpringDataJpaItemRepository.java
+++ src/main/java/hello/itemservice/repository/jpa/SpringDataJpaItemRepository.java
@@ -1,7 +1,6 @@
 package hello.itemservice.repository.jpa;
 
 import hello.itemservice.domain.Item;
-import hello.itemservice.repository.ItemRepository;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
