c5ca6799ce30fcc29111442f757061b4106c4714
aka-nick <bechalrang@gmail.com>
2023-03-14 12:57:17 +0900

    섹션4 - 데이터 접근 기술 - QueryDSL - 트레이드오프 한 구조로 리팩토링(두 Repository를 의존할 Service class 구현)

---
+++ src/main/java/hello/itemservice/service/ItemServiceV2.java
@@ -0,0 +1,44 @@
+package hello.itemservice.service;
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.repository.ItemSearchCond;
+import hello.itemservice.repository.ItemUpdateDto;
+import hello.itemservice.repository.v2.ItemQueryRepositoryV2;
+import hello.itemservice.repository.v2.ItemRepositoryV2;
+import java.util.List;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class ItemServiceV2 implements ItemService {
+
+    private final ItemRepositoryV2 repository;
+    private final ItemQueryRepositoryV2 queryRepository;
+
+    @Override
+    public Item save(Item item) {
+        return repository.save(item);
+    }
+
+    @Override
+    public void update(Long itemId, ItemUpdateDto updateParam) {
+        Item find = repository.findById(itemId).orElseThrow();
+        find.setItemName(updateParam.getItemName());
+        find.setPrice(updateParam.getPrice());
+        find.setQuantity(updateParam.getQuantity());
+    }
+
+    @Override
+    public Optional<Item> findById(Long id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    public List<Item> findItems(ItemSearchCond cond) {
+        return queryRepository.findAll(cond);
+    }
+}
